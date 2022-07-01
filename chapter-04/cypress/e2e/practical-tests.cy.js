104aeb559a478e568b9eb8f23a2c493d1500f6d2
Mauro Battello <mauro.battello@ad.rgigroup.com>
2022-07-01 16:26:11 +0200

    aggiornamento dopo ultimo practice exercise di chapter-04

--- chapter-04/cypress/e2e/practical-tests.cy.js
+++ chapter-04/cypress/e2e/practical-tests.cy.js
@@ -1,16 +1,16 @@
 describe('Todo Application tests', () => {
-    it('Visits the Todo application', () => {
+    it.skip('Visits the Todo application', () => {
         cy.visit('http://todomvc.com/examples/react/#/')
     });
-    it('Contains todo input element', () => {
+    it.skip('Contains todo input element', () => {
         cy.visit('http://todomvc.com/examples/react/#/')
         cy.get('.new-todo')
     });
-    it('Adds a new todo', () => {
+    it.skip('Adds a new todo', () => {
         cy.visit('http://todomvc.com/examples/react/#/')
         cy.get('.new-todo').type('New Todo {enter}')
     });
-    it('asserts change in application state', () => {
+    it.skip('asserts change in application state', () => {
         cy.visit('http://todomvc.com/examples/react/#/')
        
         cy.get('.new-todo').type('New Todo {enter}')
@@ -22,8 +22,12 @@ describe('Todo Application tests', () => {
        
         cy.get('.new-todo').type('New Todo {enter}')
         cy.get('.new-todo').type('Another Todo {enter}')
-        cy.get(".todo-list").find('li').should('have.length', 2)
+        cy.get('.new-todo').type('third one {enter}')
+        cy.get('.new-todo').type('4th one {enter}')
+        cy.get(".todo-list").find('li').should('have.length', 4)
         cy.get('li:nth-child(1)>div>label').should('have.text', 'New Todo')
         cy.get('li:nth-child(2)>div>label').should('have.text', 'Another Todo')
+        cy.get('li:nth-child(3)>div>label').should('have.text', 'third one')
+        cy.get('li:nth-child(4)>div>label').should('have.text', '4th one')
     });
 });
